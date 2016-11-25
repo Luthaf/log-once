8f9f3012d37e51cb92f7076a1bf6cbc5292e019a
Guillaume Fraux <guillaume.fraux@ens.fr>
2016-11-25 14:00:04 +0100

    Add documentation for all macros

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,40 @@
+//! Helper macros for logging some events only once.
+//!
+//! # Examples
+//!
+//! ```rust, no_run
+//! #[macro_use]
+//! extern crate log;
+//! #[macro_use]
+//! extern crate log_once;
+//!
+//! # #[derive(Debug)] pub struct Yak(String);
+//! # impl Yak { fn shave(&self, _: u32) {} }
+//! # fn find_a_razor() -> Result<u32, u32> { Ok(1) }
+//! pub fn shave_the_yak(yaks: &[Yak]) {
+//!     info!(target: "yak_events", "Commencing yak shaving for {:?}", yak);
+//!
+//!     for yak in yaks {
+//!         loop {
+//!             match find_a_razor() {
+//!                 Ok(razor) => {
+//!                     // This will only appear once in the logger output for each razor
+//!                     info_once!("Razor located: {}", razor);
+//!                     yak.shave(razor);
+//!                     break;
+//!                 }
+//!                 Err(err) => {
+//!                     // This will only appear once in the logger output for each error
+//!                     warn_once!("Unable to locate a razor: {}, retrying", err);
+//!                 }
+//!             }
+//!         }
+//!     }
+//! }
+//!
+//! # fn main() {}
+//! ```
+
 #[macro_use]
 extern crate log;
 #[macro_use]
@@ -5,6 +42,15 @@ extern crate lazy_static;
 
 pub use log::LogLevel;
 
+/// Standard logging macro, logging events once for each arguments.
+///
+/// The log event will only be emmited once for each combinaison of target/arguments.
+///
+/// This macro will generically log with the specified `LogLevel` and `format!`
+/// based argument list.
+///
+/// The `max_level_*` features can be used to statically disable logging at
+/// various levels.
 #[macro_export]
 macro_rules! log_once {
     (target: $target:expr, $lvl:expr, $($arg:tt)+) => ({
@@ -23,6 +69,11 @@ macro_rules! log_once {
     ($lvl:expr, $($arg:tt)+) => (log_once!(target: module_path!(), $lvl, $($arg)+))
 }
 
+/// Logs a message once at the error level.
+///
+/// The log event will only be emmited once for each combinaison of target/arguments.
+///
+/// Logging at this level is disabled if the `max_level_off` feature is present.
 #[macro_export]
 macro_rules! error_once {
     (target: $target:expr, $($arg:tt)*) => (
@@ -33,6 +84,16 @@ macro_rules! error_once {
     )
 }
 
+/// Logs a message once at the warn level.
+///
+/// The log event will only be emmited once for each combinaison of target/arguments.
+///
+/// Logging at this level is disabled if any of the following features are
+/// present: `max_level_off` or `max_level_error`.
+///
+/// When building in release mode (i.e., without the `debug_assertions` option),
+/// logging at this level is also disabled if any of the following features are
+/// present: `release_max_level_off` or `max_level_error`.
 #[macro_export]
 macro_rules! warn_once {
     (target: $target:expr, $($arg:tt)*) => (
@@ -43,6 +104,17 @@ macro_rules! warn_once {
     )
 }
 
+/// Logs a message once at the info level.
+///
+/// The log event will only be emmited once for each combinaison of target/arguments.
+///
+/// Logging at this level is disabled if any of the following features are
+/// present: `max_level_off`, `max_level_error`, or `max_level_warn`.
+///
+/// When building in release mode (i.e., without the `debug_assertions` option),
+/// logging at this level is also disabled if any of the following features are
+/// present: `release_max_level_off`, `release_max_level_error`, or
+/// `release_max_level_warn`.
 #[macro_export]
 macro_rules! info_once {
     (target: $target:expr, $($arg:tt)*) => (
@@ -53,6 +125,18 @@ macro_rules! info_once {
     )
 }
 
+/// Logs a message once at the debug level.
+///
+/// The log event will only be emmited once for each combinaison of target/arguments.
+///
+/// Logging at this level is disabled if any of the following features are
+/// present: `max_level_off`, `max_level_error`, `max_level_warn`, or
+/// `max_level_info`.
+///
+/// When building in release mode (i.e., without the `debug_assertions` option),
+/// logging at this level is also disabled if any of the following features are
+/// present: `release_max_level_off`, `release_max_level_error`,
+/// `release_max_level_warn`, or `release_max_level_info`.
 #[macro_export]
 macro_rules! debug_once {
     (target: $target:expr, $($arg:tt)*) => (
@@ -63,6 +147,19 @@ macro_rules! debug_once {
     )
 }
 
+/// Logs a message once at the trace level.
+///
+/// The log event will only be emmited once for each combinaison of target/arguments.
+///
+/// Logging at this level is disabled if any of the following features are
+/// present: `max_level_off`, `max_level_error`, `max_level_warn`,
+/// `max_level_info`, or `max_level_debug`.
+///
+/// When building in release mode (i.e., without the `debug_assertions` option),
+/// logging at this level is also disabled if any of the following features are
+/// present: `release_max_level_off`, `release_max_level_error`,
+/// `release_max_level_warn`, `release_max_level_info`, or
+/// `release_max_level_debug`.
 #[macro_export]
 macro_rules! trace_once {
     (target: $target:expr, $($arg:tt)*) => (
